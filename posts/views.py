4c2e34f5382377e52aab55b5ff30f3fa137690fd
takumiiimo <[assistant@gmail.com]>
2021-05-01 23:21:46 +0900

    環境構築

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    # return HttpResponse('Hello World!　このページは投稿のインデックスです。')
+    return render(request, 'posts/index.html')
+
+# Create your views here.
