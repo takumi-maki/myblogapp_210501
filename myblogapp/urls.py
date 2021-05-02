6edb350791a160597d7aeee11224af4db753740b
takumiiimo <[assistant@gmail.com]>
2021-05-02 14:19:27 +0900

    詳細ページの編集

--- myblogapp/urls.py
+++ myblogapp/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from posts import views
 
 urlpatterns = [
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+
+# Create your views here.
 
 
 def index(request):
@@ -8,4 +10,7 @@ def index(request):
     posts = Post.objects.order_by('-published')
     return render(request, 'posts/index.html', {'posts': posts})
 
-# Create your views here.
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post': post})
