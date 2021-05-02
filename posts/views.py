99047c16e551fa24cb74989d2d6c8e196ca4144a
takumiiimo <[assistant@gmail.com]>
2021-05-02 11:57:14 +0900

    管理画面の編集

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
     # return HttpResponse('Hello World!　このページは投稿のインデックスです。')
-    return render(request, 'posts/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
 
 # Create your views here.
